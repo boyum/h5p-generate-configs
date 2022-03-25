8f6f8eea4886d7b54d9191ad544abb550bfe7d44
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-03-25 19:48:51 +0100

    fix test

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -2,7 +2,7 @@ import { system, filesystem } from "gluegun";
 
 const src = filesystem.path(__dirname, "..");
 
-const cli = async cmd =>
+const cli = async (cmd: string) =>
   system.run(
     "node " + filesystem.path(src, "bin", "h5p-generate-configs") + ` ${cmd}`,
   );
