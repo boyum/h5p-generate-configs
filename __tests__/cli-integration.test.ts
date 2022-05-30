42f9d466b1ab2dd6c14e375c2e565f4d4f618476
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-05-30 15:57:28 +0200

    create integration test

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -1,3 +1,4 @@
+import { promises as fs } from "fs";
 import { system, filesystem } from "gluegun";
 
 const src = filesystem.path(__dirname, "..");
@@ -7,14 +8,30 @@ const cli = async (cmd: string) =>
     "node " + filesystem.path(src, "bin", "h5p-generate-configs") + ` ${cmd}`,
   );
 
-test("outputs version", async () => {
-  const output = await cli("--version");
-  expect(output).toContain("0.0.1");
-});
+test("generates semantics.json", async () => {
+  const tempDir = await fs.mkdtemp("semantics-test_");
+  await fs.copyFile("demo/semantics.ts", `${tempDir}/semantics.ts`);
+  await fs.copyFile(
+    "demo/semantics-helper.ts",
+    `${tempDir}/semantics-helper.ts`,
+  );
 
-test("outputs help", async () => {
-  const output = await cli("--help");
-  expect(output).toContain("0.0.1");
+  try {
+    await cli(
+      `s -s ${tempDir}/semantics.ts --semanticsOut ${tempDir}/semantics.json -t ${tempDir}/TranslationKey.ts`,
+    );
+
+    const expected = (await fs.readFile("demo/semantics.json")).toString(
+      "utf-8",
+    );
+    const actual = (await fs.readFile(`${tempDir}/semantics.json`)).toString(
+      "utf-8",
+    );
+
+    expect(actual).toBe(expected);
+  } finally {
+    await fs.rm(tempDir, { recursive: true });
+  }
 });
 
 // test("generates file", async () => {
